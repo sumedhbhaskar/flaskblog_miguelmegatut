f8846bacf63cc3f1f6e818067b9af510c4efc440
sumedhbhaskar <sumedh.bhaskar@gmail.com>
2020-05-24 05:36:04 +0530

    added login template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
-from flask import render_template
-
+from flask import render_template, redirect, flash, url_for
+from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
@@ -16,3 +16,13 @@ def index():
                 }
             ]
     return render_template('index.html',title='Home',user=user,posts=posts)
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Yo! you logined with name {}, remember me =  {}'.format(form.username.data,form.remember_me.data))
+        return redirect(url_for('index'))
+
+    return render_template('login.html',title='Sign In',form=form)
