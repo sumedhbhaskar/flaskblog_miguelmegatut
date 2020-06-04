08bd81f60c91809ed1759bed9e227257063fa702
sumedhbhaskar <sumedh.bhaskar@gmail.com>
2020-06-04 05:43:51 +0530

    email support

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+from flask_mail import Mail
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,5 +12,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
+moment = Moment(app)
 
 from app import routes, models, errors
